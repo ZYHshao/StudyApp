ba42f03d8e843583eaca47cc338fe04681953132
ZYHshao <apple@appledeMacBook-Air.local>
2015-09-13 23:16:07 +0800

    修改

--- StudyApp/Controllers/Views/Cells/YHSAWrongExamReloadTableViewCell.h
+++ StudyApp/Controllers/Views/Cells/YHSAWrongExamReloadTableViewCell.h
@@ -7,6 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+@class YHSAWrongExamReloadTableViewCell;
+@protocol YHSAWrongExamReloadCellDelegate <NSObject>
+
+-(void)clickBtn:(YHSAWrongExamReloadTableViewCell *)cell;
+
+@end
 
 @interface YHSAWrongExamReloadTableViewCell : UITableViewCell<UITextFieldDelegate>
 //题型数据源数组 这个在设计上存在一些问题 这个数组一定要最后赋值
@@ -14,4 +20,5 @@ __PROPERTY_NO_STRONG__(NSMutableArray *, dataArray);
 //用户选择题数的字典
 __PROPERTY_NO_STRONG__(NSMutableDictionary *,userDic);
 __PROPERTY_NO_STRONG__(NSString *, theTitle);
+__PROPERTY_NO_WEAK__(id<YHSAWrongExamReloadCellDelegate>, delegate);
 @end
